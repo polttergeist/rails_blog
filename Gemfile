e88b23af791e0f9cddd551f1f239003d4df40a77
polttergeist <ivan010201@gmail.com>
2022-09-13 01:13:42 +0300

    all the way to step 27, added sql file (totally unrelated)

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "jquery-rails"
 gem "wicked_pdf"
 gem "wkhtmltopdf-binary"
 gem "pg"
+gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_10_225022) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_10_231507) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -21,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_10_225022) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
